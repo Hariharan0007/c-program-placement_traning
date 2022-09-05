c246348a477a3c2f8d7fe33af2d86ea4cbaf0018
unknown <hariharanhighlevelprogrammer@gmail.com>
2022-09-05 23:57:27 +0530

    plct

--- table.c
+++ table.c
@@ -1,16 +1,23 @@
 #include<stdio.h>
 void main()
 {
-	int n;
+	int n,count=0;
 	printf("Enter the number : ");
 	scanf("%d",&n);
-	int i;
+	int i,temp;
 	char c='*';
-	for(i=1;i<=10;i++)
+	for(i=1;i<=50;i++)
 	{
-		if(i==10)
+		temp =i;
+		do
+		{
+			temp/=10;
+			++count;
+		}while(temp!=0);
+		if(count>1)
 		printf("%2d %c %d = %d\n",i,c,n,n*i);
 		else
 		printf("%d %2c %d = %d\n",i,c,n,n*i);
+		count = 0;
 	}
 }
